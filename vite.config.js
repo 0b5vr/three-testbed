e891c8b95ce61179b79ebc9a7a29267911d9bdba
0b5vr <0b5vr@0b5vr.com>
2023-10-25 00:06:18 +0900

    chore: fix build / ci ecosystem

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,23 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
-export default defineConfig({
-  root: resolve(__dirname, 'src', 'pages'),
-});
+console.log(resolve( __dirname, 'src/pages/index.html' ));
+
+export default defineConfig( {
+  root: resolve( __dirname, 'src/pages' ),
+  base: './',
+  build: {
+    outDir: resolve( __dirname, 'dist' ),
+    rollupOptions: {
+      input: {
+        main: resolve( __dirname, 'src/pages/index.html' ),
+        bvh: resolve( __dirname, 'src/pages/bvh/index.html' ),
+        cube: resolve( __dirname, 'src/pages/cube/index.html' ),
+        fbx: resolve( __dirname, 'src/pages/fbx/index.html' ),
+        fsq: resolve( __dirname, 'src/pages/fsq/index.html' ),
+        gltf: resolve( __dirname, 'src/pages/gltf/index.html' ),
+        vrm: resolve( __dirname, 'src/pages/vrm/index.html' ),
+      },
+    },
+  },
+} );
